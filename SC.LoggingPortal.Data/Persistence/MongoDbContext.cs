91e984474d83da2c38489a59074cc886b5c92e50
Younes van Ruth <y.vanruth@estate.nl>
2015-06-25 11:06:57 +0200

    Mongo C# Driver

--- SC.LoggingPortal.Data/Persistence/MongoDbContext.cs
+++ SC.LoggingPortal.Data/Persistence/MongoDbContext.cs
@@ -19,6 +19,43 @@
 
         internal readonly string dbName;
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MongoDbContext"/> class.
+        /// </summary>
+        /// <param name="connectionStringOrName">Name of the connection string or.</param>
+        public MongoDbContext(string connectionStringOrName)
+        {
+            this.client = GetClient(connectionStringOrName);
+            this.dbName = GetDatabaseName(connectionStringOrName);
+            this.database = GetDatabase(connectionStringOrName);
+        }
+
+        private string GetDatabaseName(string connectionStringOrName)
+        {
+            Uri connectionStringUri = TryGetConnectionString(connectionStringOrName);
+            return connectionStringUri.Segments.Where(segment => segment != "/").FirstOrDefault();
+        }
+
+        private MongoClient GetClient(string connectionStringOrName)
+        {
+            Uri connectionStringUri = TryGetConnectionString(connectionStringOrName);
+            var mongoConnection = string.Concat(connectionStringUri.Scheme, "://", connectionStringUri.Authority);
+            var mongoClient = new MongoClient(mongoConnection);
+            return mongoClient;
+        }
+
+        /// <summary>
+        /// Gets the database.
+        /// </summary>
+        /// <param name="connectionStringOrName">Name of the connection string or.</param>
+        /// <returns>MongoDatabase.</returns>
+        private MongoDatabaseBase GetDatabase(string connectionStringOrName)
+        {
+            var mongoServer = this.client.GetServer();
+            return mongoServer.GetDatabase(this.dbName);
+        }
+
         /// <summary>
         /// Tries the get connection string.
         /// </summary>
