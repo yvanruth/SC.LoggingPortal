0770e4acc675fa7304b119fbe615d280ac5e72d5
Younes van Ruth <y.vanruth@estate.nl>
2015-12-09 17:04:11 +0100

    fail check in Logger + webservice timeout in config of service

--- SC.LoggingPortal.LogAppender/Log4NetMongoAppender.cs
+++ SC.LoggingPortal.LogAppender/Log4NetMongoAppender.cs
@@ -12,10 +12,39 @@
     public class Log4NetMongoAppender : FileAppender
     {
         private readonly SCLogger _logger = new SCLogger();
-        
+
+        private bool LoggingDisabled
+        {
+            get
+            {
+                HttpContext.Current.Cache.Insert("Absolute Cache expiration 10 mins / 5 mins.")
+                if (HttpContext.Current.Cache["sc.loggingportal.isloggingdisabled"] != null)
+                {
+                    return (bool)HttpContext.Current.Cache["sc.loggingportal.isloggingdisabled"];
+                }
+
+                return false;
+            }
+
+            set
+            {
+                HttpContext.Current.Cache["sc.loggingportal.isloggingdisabled"] = value;
+            }
+        }
+
         protected override void Append(LoggingEvent loggingEvent)
         {
-            this._logger.LogMessage(new LogMessage { ApplicationName = System.Web.Hosting.HostingEnvironment.SiteName, IPAddress = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString(), Is64BitProcess = Environment.Is64BitProcess, LoggerMessage = loggingEvent.RenderedMessage, LoggerName = loggingEvent.LoggerName, LogLevel = loggingEvent.Level.Name, LogUserIdentity = loggingEvent.UserName, MachineName = Environment.MachineName, NetVersion = Environment.Version.ToString(), TimeStamp = loggingEvent.TimeStamp });            
+            if (!LoggingDisabled)
+            {
+                try
+                {
+                    this._logger.LogMessage(new LogMessage { ApplicationName = System.Web.Hosting.HostingEnvironment.SiteName, IPAddress = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString(), Is64BitProcess = Environment.Is64BitProcess, LoggerMessage = loggingEvent.RenderedMessage, LoggerName = loggingEvent.LoggerName, LogLevel = loggingEvent.Level.Name, LogUserIdentity = loggingEvent.UserName, MachineName = Environment.MachineName, NetVersion = Environment.Version.ToString(), TimeStamp = loggingEvent.TimeStamp });
+                }
+                catch (Exception ex)
+                {
+                    LoggingDisabled = true;
+                }   
+            }                    
         }
     }
 }
